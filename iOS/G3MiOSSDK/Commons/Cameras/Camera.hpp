3c51e9af8a4258b087a1dcf6352f9b623378b076
Agustín Trujillo Pino <gus@imac-de-agustin-trujillo-pino.local>
2012-09-10 21:06:02 +0100

    activating wireframe + halffrustum

--- iOS/G3MiOSSDK/Commons/Cameras/Camera.hpp
+++ iOS/G3MiOSSDK/Commons/Cameras/Camera.hpp
@@ -358,8 +358,11 @@ private:
       if (_halfFrustum!=NULL) delete _halfFrustum;
 #endif
       FrustumData data = getFrustumData();
-      _halfFrustum = new Frustum(data._left/2, data._right/2,
+      /*_halfFrustum = new Frustum(data._left/2, data._right/2,
                                  data._bottom/2, data._top/2,
+                                 data._znear, data._zfar);*/
+      _halfFrustum = new Frustum(data._left/4, data._right/4,
+                                 data._bottom/4, data._top/4,
                                  data._znear, data._zfar);
     }
     return _halfFrustum;
