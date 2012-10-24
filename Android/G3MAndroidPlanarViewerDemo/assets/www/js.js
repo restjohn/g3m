9e3e93b7aeb4d82bfd18541cafa3aa5578d3eefd
fpulido <fpulido@becario.(none)>
2012-10-24 13:13:26 +0200

    android planar viewer demo

---
+++ Android/G3MAndroidPlanarViewerDemo/assets/www/js.js
@@ -0,0 +1,7 @@
+function hello(){
+    	var label = document.createElement("label");
+    	label.innerHTML = "hooola";
+    	document.body.appendChild(label);
+    	alert("hola");
+    }
+
---
+++ Android/G3MAndroidPlanarViewerDemo/gen/org/glob3/mobile/g3mandroidplanarviewerdemo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.glob3.mobile.g3mandroidplanarviewerdemo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
